b3ffe506c6d693aaa160783dc3dcb63e0d7532e9
takaczapka <takaczapka@gmail.com>
2016-11-13 11:17:36 +0000

    clarified docs and added run.sh script

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+set -e
+
 # --cors Enable CORS via the Access-Control-Allow-Origin header
 # (the Cross-Origin Resource Sharing (CORS) mechanism gives web servers cross-domain access 
 # controls, which enable secure cross-domain data transfers. 
+./http-server/bin/http-server --cors logo/ &
 
-./http-server/bin/http-server --cors logo/
+open jslogo/index.html &
 
+wait
 
